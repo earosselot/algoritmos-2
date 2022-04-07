94f1936fc697d3bf4e58b25b49cbee4bff55e0cb
Eduardo Rosselot <eduardo.rosselot@flexia.com.ar>
2022-04-07 17:07:55 -0300

    labo3

--- Laboratorio/labos/03-ejercitacion_compilacion_testing/tests/ej_testing.cpp
+++ Laboratorio/labos/03-ejercitacion_compilacion_testing/tests/ej_testing.cpp
@@ -8,10 +8,56 @@ using namespace std;
 
 // Ejercicio 4
 TEST(Aritmetica, suma) {
+    int resultado = 15 + 7;
+    int resultadoEsperado = 22;
+    EXPECT_EQ(resultado, resultadoEsperado);
 }
 
 // Ejercicio 5
-TEST( ... ) {
+TEST(Aritmetica, potencia) {
+    int resultado = pow(10, 2);
+    int resultadoEsperado = 100;
+    EXPECT_EQ(resultado, resultadoEsperado);
+}
+
+TEST(Aritmetica, potencia_general) {
+    for (int num = -5; num <= 6; num++) {
+        int resultado = pow(num, 2);
+        int resultadoEsperado = num * num;
+        EXPECT_EQ(resultado, resultadoEsperado);
+    }
 }
 
 // Ejercicios 6..9
+TEST(Diccionario, obtener) {
+    map<int, int> dicEnteros;
+    dicEnteros.insert(make_pair(6, 14));
+    ASSERT_EQ(dicEnteros[6], 14);
+}
+
+TEST(Diccionario, definir) {
+    map<int, int> dicEnteros;
+    int count6 = dicEnteros.count(6);
+    ASSERT_EQ(count6, 0);
+    dicEnteros.insert(make_pair(6, 7));
+    count6 = dicEnteros.count(6);
+    ASSERT_EQ(count6, 1);
+}
+
+TEST(Truco, inicio) {
+    Truco truco = Truco();
+    EXPECT_EQ(truco.puntaje_j1(), 0);
+    EXPECT_EQ(truco.puntaje_j2(), 0);
+}
+
+TEST(Truco, buenas) {
+    Truco truco = Truco();
+    EXPECT_FALSE(truco.buenas(1));
+    for(int i = 1; i < 16; i++) {
+        truco.sumar_punto(1);
+    }
+    EXPECT_FALSE(truco.buenas(1));
+    truco.sumar_punto(1);
+    EXPECT_TRUE(truco.buenas(1));
+}
+s82ns0pwLRjj*8
