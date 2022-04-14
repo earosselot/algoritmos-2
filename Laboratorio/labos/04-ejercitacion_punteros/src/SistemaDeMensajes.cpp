e4050e5b6827d299fb431ddbb99d8eff6c9654ae
Eduardo Rosselot <eduardo.rosselot@flexia.com.ar>
2022-04-14 01:08:09 -0300

    labo 4 v2

--- Laboratorio/labos/04-ejercitacion_punteros/src/SistemaDeMensajes.cpp
+++ Laboratorio/labos/04-ejercitacion_punteros/src/SistemaDeMensajes.cpp
@@ -1,54 +1,122 @@
 #include "SistemaDeMensajes.h"
 
-//SistemaDeMensajes::SistemaDeMensajes(): _conns() {}
-//
-//void SistemaDeMensajes::registrarJugador(int id, string ip) {
-////    ConexionJugador* conexionJugador = new ConexionJugador;
-////    ConexionJugador jugador(ip);
-//    this->_conns[id] = new ConexionJugador(ip);
-//}
-//
-//bool SistemaDeMensajes::registrado(int id) const {
-//    return this->_conns[id] != nullptr;
-//}
-//
-//void SistemaDeMensajes::enviarMensaje(int id, string mensaje) {
-//    this->_conns[id]->enviarMensaje(mensaje);
-//    // otras opciones que hacen lo mismo:
-//    // ConexionJugador* conexionJugador = this->_conns[id];
-//    // a:
-//    // (*conexionJugador).enviarMensaje(mensaje);
-//    // b:
-//    // conexionJugador->enviarMensaje(mensaje);
-//}
-//
-//string SistemaDeMensajes::ipJugador(int id) const {
-//    return this->_conns[id]->ip();
-//}
-//
-//// todo: ejercicio2 esta bien esto?
-//// delete[] _conns;
-//// delete _conns;
-//// ninguno de estos funcionaba
-//SistemaDeMensajes::~SistemaDeMensajes() {
-//    for (ConexionJugador* conn: this->_conns) {
-//        delete conn;
-//    }
-//    for (Proxy* proxy: this->_proxys) {
-//        delete proxy;
-//    }
-//}
-//
-//void SistemaDeMensajes::desregistrarJugador(int id) {
-//    this->_conns[id] = nullptr;
-//}
-//
-//Proxy* SistemaDeMensajes::obtenerProxy(int id) {
-//    _proxys.push_back(new Proxy(&(this->_conns[id])));
-//    return _proxys.back();
-//}
+#if EJ == 1 || EJ == 2 || EJ == 3
+
+SistemaDeMensajes::SistemaDeMensajes(): _conns() {}
+
+void SistemaDeMensajes::registrarJugador(int id, string ip) {
+    this->_conns[id] = new ConexionJugador(ip);
+}
+
+bool SistemaDeMensajes::registrado(int id) const {
+    return this->_conns[id] != nullptr;
+}
+
+void SistemaDeMensajes::enviarMensaje(int id, string mensaje) {
+    this->_conns[id]->enviarMensaje(mensaje);
+    // otras opciones que hacen lo mismo:
+    // ConexionJugador* conexionJugador = this->_conns[id];
+    // a:
+    // (*conexionJugador).enviarMensaje(mensaje);
+    // b:
+    // conexionJugador->enviarMensaje(mensaje);
+}
+
+string SistemaDeMensajes::ipJugador(int id) const {
+    return this->_conns[id]->ip();
+}
+
+SistemaDeMensajes::~SistemaDeMensajes() {
+    for (ConexionJugador* conn: this->_conns) {
+        delete conn;
+    }
+}
+
+void SistemaDeMensajes::desregistrarJugador(int id) {
+    this->_conns[id] = nullptr;
+}
+
 // todo: falta correr ej 2 y 3 con valgrind
 
+
+#elif EJ == 4 || EJ == 5
+
+SistemaDeMensajes::SistemaDeMensajes(): _conns() {}
+
+void SistemaDeMensajes::registrarJugador(int id, string ip) {
+    this->_conns[id] = new ConexionJugador(ip);
+}
+
+bool SistemaDeMensajes::registrado(int id) const {
+    return this->_conns[id] != nullptr;
+}
+
+void SistemaDeMensajes::enviarMensaje(int id, string mensaje) {
+    this->_conns[id]->enviarMensaje(mensaje);
+}
+
+string SistemaDeMensajes::ipJugador(int id) const {
+    return this->_conns[id]->ip();
+}
+
+SistemaDeMensajes::~SistemaDeMensajes() {
+    for (ConexionJugador* conn: this->_conns) {
+        delete conn;
+    }
+    for (Proxy* proxy: this->_proxys) {
+        delete proxy;
+    }
+}
+
+void SistemaDeMensajes::desregistrarJugador(int id) {
+    this->_conns[id] = nullptr;
+}
+
+Proxy* SistemaDeMensajes::obtenerProxy(int id) {
+    _proxys.push_back(new Proxy(this->_conns[id]));
+    return _proxys.back();
+}
+// todo: falta correr ej 4 y 5 con valgrind
+
+#elif EJ == 6
+
+SistemaDeMensajes::SistemaDeMensajes(): _conns() {}
+
+void SistemaDeMensajes::registrarJugador(int id, string ip) {
+    this->_conns[id] = new ConexionJugador(ip);
+}
+
+bool SistemaDeMensajes::registrado(int id) const {
+    return this->_conns[id] != nullptr;
+}
+
+void SistemaDeMensajes::enviarMensaje(int id, string mensaje) {
+    this->_conns[id]->enviarMensaje(mensaje);
+}
+
+string SistemaDeMensajes::ipJugador(int id) const {
+    return this->_conns[id]->ip();
+}
+
+SistemaDeMensajes::~SistemaDeMensajes() {
+    for (ConexionJugador* conn: this->_conns) {
+        delete conn;
+    }
+    for (Proxy* proxy: this->_proxys) {
+        delete proxy;
+    }
+}
+
+void SistemaDeMensajes::desregistrarJugador(int id) {
+    this->_conns[id] = nullptr;
+}
+
+Proxy* SistemaDeMensajes::obtenerProxy(int id) {
+    _proxys.push_back(new Proxy(&(this->_conns[id])));
+    return _proxys.back();
+}
+
+#elif EJ == 7
 
 //  ----------- Ejercicio 7 -----------------------
 
@@ -70,10 +138,6 @@ string SistemaDeMensajes::ipJugador(int id) const {
     return this->_conns[id]->ip();
 }
 
-// todo: ejercicio2 esta bien esto?
-// delete[] _conns;
-// delete _conns;
-// ninguno de estos funcionaba
 SistemaDeMensajes::~SistemaDeMensajes() {
     for (ConexionJugador* conn: this->_conns) {
         delete conn;
@@ -97,3 +161,10 @@ SistemaDeMensajes::Proxy::Proxy(ConexionJugador** conn): _conn(conn) {}
 void SistemaDeMensajes::Proxy::enviarMensaje(string msg) {
     (**_conn).enviarMensaje(msg);
 }
+
+#endif
+
+
+
+
+
--- Laboratorio/labos/04-ejercitacion_punteros/src/SistemaDeMensajes.h
+++ Laboratorio/labos/04-ejercitacion_punteros/src/SistemaDeMensajes.h
@@ -5,43 +5,95 @@
 #include <string>
 #include <vector>
 
-#if EJ == 4 || EJ == 5
-#include "Proxy.h"
-#elif EJ == 6
-#include "Proxy2.h"
-#endif
-
 using namespace std;
 
-//class SistemaDeMensajes {
-//  public:
-//    SistemaDeMensajes();
-//    // Pre: 0 <= id < 4
-//    void registrarJugador(int id, string ip);
-//    // Pre: 0 <= id < 4
-//    bool registrado(int id) const;
-//
-//    // Pre: registrado(id)
-//    void enviarMensaje(int id, string mensaje);
-//
-//    // Pre: registrado(id)
-//    string ipJugador(int id) const;
-//
-//    void desregistrarJugador(int id);
-//
-//    ~SistemaDeMensajes();
-//
-//    // ej4
-////    Proxy* obtenerProxy(int id);
-//
-//    // ej6
-//    Proxy* obtenerProxy(int id);
-//
-//private:
-//    ConexionJugador* _conns[4];
-//    vector<Proxy*> _proxys;
-//};
+#if EJ == 1 || EJ == 2 || EJ == 3
 
+//  ----------- Ejercicio 1 a 3 -----------------------
+
+class SistemaDeMensajes {
+public:
+    SistemaDeMensajes();
+    // Pre: 0 <= id < 4
+    void registrarJugador(int id, string ip);
+    // Pre: 0 <= id < 4
+    bool registrado(int id) const;
+
+    // Pre: registrado(id)
+    void enviarMensaje(int id, string mensaje);
+
+    // Pre: registrado(id)
+    string ipJugador(int id) const;
+
+    void desregistrarJugador(int id);
+
+    ~SistemaDeMensajes();
+
+private:
+    ConexionJugador* _conns[4];
+};
+
+#elif EJ == 4 || EJ == 5
+#include "Proxy.h"
+
+//  ----------- Ejercicio 4 -5 -----------------------
+
+class SistemaDeMensajes {
+  public:
+    SistemaDeMensajes();
+    // Pre: 0 <= id < 4
+    void registrarJugador(int id, string ip);
+    // Pre: 0 <= id < 4
+    bool registrado(int id) const;
+
+    // Pre: registrado(id)
+    void enviarMensaje(int id, string mensaje);
+
+    // Pre: registrado(id)
+    string ipJugador(int id) const;
+
+    void desregistrarJugador(int id);
+
+    ~SistemaDeMensajes();
+
+    Proxy* obtenerProxy(int id);
+
+private:
+    ConexionJugador* _conns[4];
+    vector<Proxy*> _proxys;
+};
+
+#elif EJ == 6
+#include "Proxy2.h"
+
+//  ----------- Ejercicio 6 -----------------------
+
+class SistemaDeMensajes {
+  public:
+    SistemaDeMensajes();
+    // Pre: 0 <= id < 4
+    void registrarJugador(int id, string ip);
+    // Pre: 0 <= id < 4
+    bool registrado(int id) const;
+
+    // Pre: registrado(id)
+    void enviarMensaje(int id, string mensaje);
+
+    // Pre: registrado(id)
+    string ipJugador(int id) const;
+
+    void desregistrarJugador(int id);
+
+    ~SistemaDeMensajes();
+
+    Proxy* obtenerProxy(int id);
+
+private:
+    ConexionJugador* _conns[4];
+    vector<Proxy*> _proxys;
+};
+
+#elif EJ == 7
 
 //  ----------- Ejercicio 7 -----------------------
 
@@ -83,5 +135,7 @@ private:
     ConexionJugador* _conns[4];
     vector<Proxy*> _proxys;
 };
+
+#endif
 
 #endif
